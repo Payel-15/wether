7c4c5b1b9041de2bf14306c5d48ce5b35b7f23a7
Payel-15 <payelmondal150405@gmail.com>
2025-07-25 13:23:39 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,37 @@
+import requests
+import pyttsx3
+
+engine = pyttsx3.init()
+
+def speak(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
+def get_weather(city_name):
+    api_key = "openweathermap_api_key" #api laga le
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+        "q": city_name,
+        "appid": api_key,
+        "units": "metric"
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if data["cod"] != 200:
+            speak(f"City '{city_name}' not found.")
+        else:
+            temp = data["main"]["temp"]
+            desc = data["weather"][0]["description"]
+            speak(f"The current temperature in {city_name} is {temp}°C with {desc}.")
+    except Exception as e:
+        speak("Unable to fetch weather data. Please check your connection or API key.")
+        print(e)
+
+if __name__ == "__main__":
+    city = input("Enter city name: ")
+    get_weather(city)
